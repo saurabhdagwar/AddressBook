b7f5aa3869aef8147499fc31589e2352537179a4
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-09-18 23:39:08 +0530

    [Saurabh]Added: Worked with multiple contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class AddressBook {
     static Scanner scan = new Scanner(System.in);
     public static ArrayList<ContactPerson> people = new ArrayList<ContactPerson>();
-    static final int ONE = 1, TWO = 2;
+    static final int ZERO = 0, ONE = 1, TWO = 2;
 
     public static void main(String args[]) {
         int choice = 0;
         AddressBook contact = new AddressBook();
         System.out.println("----- Welcome to Address Book Program -----");
         while (choice < 6) {
-            System.out.println("Enter your choice " +
+            System.out.println("\nEnter your choice " +
                     "\n\t 1.Add Contact " +
                     "\n\t 2.PrintContact " +
                     "\n\t 3.Edit Contact " +
@@ -57,51 +57,54 @@ public class AddressBook {
     }
 
     public void editContact() {
-        String fname, lname, address, city, state;
-        long phone, zip;
-        int id;
-        int count = 0;
-        for (ContactPerson contact : people) {
-            System.out.println("ID: #" + people.indexOf(contact) + " : " + contact);
-        }
-        System.out.println(" Enter ID of contact to Edit : ");
-        id = scan.nextInt();
-        System.out.println(people.get(id));
-        System.out.println("What You Want to edit...\n \t 1. Address \t 2. Mobile number");
-        int choice = scan.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("Enter Address: ");
-                address = scan.next();
-                people.get(id).setAddress(address);
-                System.out.println("Enter City");
-                city = scan.next();
-                people.get(id).setCity(city);
-                System.out.println("Enter State");
-                state = scan.next();
-                people.get(id).setState(state);
-                System.out.println("Enter Zip");
-                zip = scan.nextLong();
-                people.get(id).setZip(zip);
-                break;
-            case 2:
-                System.out.println("Enter Mobile number");
-                phone = scan.nextLong();
-                people.get(id).setNumber(phone);
-                break;
-            default:
-                System.out.println("Please Enter Valid Option");
-                editContact();
+        if (people.isEmpty()) {
+            System.out.println("There are no contacts to edit ");
+        } else {
+            String fname, lname, address, city, state;
+            long phone, zip;
+            int id;
+            int count = ZERO;
+            for (ContactPerson contact : people) {
+                System.out.println("ID: #" + people.indexOf(contact) + " : " + contact);
+            }
+            System.out.println(" Enter ID of contact to Edit : ");
+            id = scan.nextInt();
+            System.out.println(people.get(id));
+            System.out.println("What You Want to edit...\n \t 1. Address \t 2. Mobile number");
+            int choice = scan.nextInt();
+            switch (choice) {
+                case ONE:
+                    System.out.println("Enter Address: ");
+                    address = scan.next();
+                    people.get(id).setAddress(address);
+                    System.out.println("Enter City");
+                    city = scan.next();
+                    people.get(id).setCity(city);
+                    System.out.println("Enter State");
+                    state = scan.next();
+                    people.get(id).setState(state);
+                    System.out.println("Enter Zip");
+                    zip = scan.nextLong();
+                    people.get(id).setZip(zip);
+                    break;
+                case TWO:
+                    System.out.println("Enter Mobile number");
+                    phone = scan.nextLong();
+                    people.get(id).setNumber(phone);
+                    break;
+                default:
+                    System.out.println("Please Enter Valid Option");
+                    editContact();
+            }
         }
     }
-
     public void deleteContact() {
         if (people.isEmpty()) {
             System.out.println("There is no contact to delete");
         } else {
             System.out.println("Enter First name to remove contact ");
             String fname = scan.next();
-            for (int count = 0; count < people.size(); count++) {
+            for (int count = ZERO; count < people.size(); count++) {
                 if (people.get(count).getfname().equals(fname)) {
                     people.remove(people.get(count));
                 }
